27f46f3c906f22023c999a56e62c8d404ea2ad91
Thomas Borge Skøien <thomabsk@stud.ntnu.no>
2021-02-23 14:25:23 +0100

    Fixed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # SIT-booke-bot
+
+
+Install virtualenv on your machine
+Run these commands to start the virtualenv
+
+```
+virtualenv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+```
+add a password.py file to your directory and add the following with your number and password
+
+```
+passw = "Your-password"
+number = "Your-number"
+
+
+Set up the schedule and you're good to go! :)
+
--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 import schedule
 import time
 import password
+
+
+print(password.number)
 def setup_schedules():
     schedule.every().monday.at("20:00").do(run_bot)
     schedule.every().monday.at("21:00").do(run_bot)
@@ -20,68 +23,74 @@ def setup_schedules():
     schedule.every().thursday.at("10:00").do(run_bot)
     schedule.every().thursday.at("11:00").do(run_bot)
 
+    schedule.every().tuesday.at("14:03").do(run_bot)
+
 def run_bot():
-    try:
-        browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
+        try:
+            browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
 
-        num_bookings = 31
-
-
-        browser.get('https://ibooking.sit.no/webapp/timeplan/?type=groupclasses&id=13&gyms=540')
-
-        page_loaded = False
-        while(not page_loaded):
-            try:
-                loginbanner = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/button[1]/i")
-                loginbanner.click()
-                
-                page_loaded = True
-            except:
-                browser.get('https://ibooking.sit.no/webapp/timeplan/?type=groupclasses&id=13&gyms=540')
+            num_bookings = 31
 
 
-        browser.switch_to.frame("auth-frame")
+            browser.get('https://ibooking.sit.no/webapp/timeplan/?type=groupclasses&id=13&gyms=540')
 
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, "username-input")))
-        mobileNumber = browser.find_element_by_id("username-input")
-        mobileNumber.send_keys(password.number)
-
-        password = browser.find_element_by_id("password-input")
-        password.send_keys(password.passw)
-
-
-        browser.find_element_by_xpath("/html/body/div/div/div[1]/div/form/button").click()
-
-
-        browser.switch_to.default_content()
-        slots = browser.find_elements_by_class_name("instance")
-        clicked = False
-        while(not clicked):
-            try:
-                slots[num_bookings].click() #Has to click twice for some reason
-                slots[num_bookings].click()
-                clicked = True
-            except:
-                pass
-
-        booked = False
-        while(not booked):
-            try:
-                book_button = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[6]/div/div/div[3]/div[8]/button[1]").click()
-                booked = True
-            except:
+            page_loaded = False
+            while(not page_loaded):
                 try:
-                    wait_list_button = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[6]/div/div/div[3]/div[8]/button[1]").click()
-                    booked = True
+                    loginbanner = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/button[1]/i")
+                    loginbanner.click()
+                    
+                    page_loaded = True
+                except:
+                    browser.get('https://ibooking.sit.no/webapp/timeplan/?type=groupclasses&id=13&gyms=540')
+
+
+            browser.switch_to.frame("auth-frame")
+
+            WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, "username-input")))
+            username_popup = False
+            mobileNumberField = browser.find_element_by_id("username-input")
+            mobileNumberField.send_keys(password.number)
+
+            passwordField = browser.find_element_by_id("password-input")
+            passwordField.send_keys(password.passw)
+
+            browser.find_element_by_xpath("/html/body/div/div/div[1]/div/form/button").click()
+
+
+            browser.switch_to.default_content()
+            slots = browser.find_elements_by_class_name("instance")
+            clicked = False
+            while(not clicked):
+                try:
+                    slots[num_bookings].click() #Has to click twice for some reason
+                    slots[num_bookings].click()
+                    clicked = True
                 except:
                     pass
 
+            booked = False
+            while(not booked):
+                try:
+                    book_button = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[6]/div/div/div[3]/div[8]/button[1]").click()
+                    booked = True
+                except:
+                    try:
+                        wait_list_button = browser.find_element_by_xpath("/html/body/div[1]/div/div/div[6]/div/div/div[3]/div[8]/button[1]").click()
+                        booked = True
+                    except:
+                        pass
 
-        print("Booking successfully made!")
-        browser.quit()
-    except:
-        print("Booking failed!")
+
+            print("Booking successfully made!")
+            browser.quit()
+        except Exception as e:
+            print("Booking failed")
+            print(e)
 setup_schedules()
-while(True):
-    schedule.run_pending()
-    time.sleep(1)
+print("Bot has started :)")
+# while(True):
+#     schedule.run_pending()
+#     time.sleep(1)
+
+run_bot()
